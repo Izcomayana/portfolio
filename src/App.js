2b6c5d9711e7085c9b4961e97bdac5a7c597b7af
Mayana <oluwapelumisotoyinbo@gmail.com>
2022-09-25 22:33:47 +0100

    font and spacing

--- src/App.js
+++ src/App.js
@@ -37,13 +37,13 @@ function App() {
 
   return (
     <div className={`App scroll-smooth  ${ toggleDarkmode && 'dark' }`}>
-      <div className="body-container transition ease-in duration-550 dark:bg-[#161616]">
+      <div className="transition ease-in duration-550 dark:bg-[#161616]">
         <div className="container px-4 md:px-8">
           <header className=''>
             <div className="cv-link dark:text-white">
-              <a href="https://docs.google.com/document/d/1BxYdgkJTpqrUSimGZCvJWUO-NzvPnZdSed9wGg1jTgw/edit#">RESUME</a>
+              <a href="https://docs.google.com/document/d/1BxYdgkJTpqrUSimGZCvJWUO-NzvPnZdSed9wGg1jTgw/edit?usp=sharing">RESUME</a>
             </div>
-            <div className="darkmode-toggle">
+            <div>
               {showDark && <div class="dark:text-white text-dark cursor-pointer text-[1.5em]" onClick={openDark} >
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 352 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                   <path d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path>
@@ -59,16 +59,16 @@ function App() {
           </header>
 
           <body>
-            <div className="about mt-4">
-              <div className="name text-center dark:text-white">
-                <img className='inline' src={me} alt="" />
+            <div className="mt-4">
+              <div className="text-center dark:text-white">
+                <img className='inline my-10' src={me} alt="" />
                 <h1 className='text-6xl font-bold pt-6 sm:text-8xl'>
                   I'm Israel.
                 </h1>
                 <p className='dark:text-[#bbbbbb] sm:text-lg my-2'>I build amazing products.</p>
               </div>
 
-              <div className="items-end ">
+              <div className="items-end my-12">
                 <div className='links flex justify-between text-center w-[80%] mt-8 m-auto sm:w-[35%] sm:mt-12'>
                   <a className='w-[10%]' href="https://www.linkedin.com/in/oluwapelumi-israel-28439a20b/"><img  src={linkedIn} alt="" /></a>
                   <a className='w-[10%]' href="https://github.com/izcomayana"><img src={github} alt="" /></a>
@@ -77,8 +77,8 @@ function App() {
                 
               </div>
 
-              <div className="intro my-8">
-                <h2 className='headers sm:text-3xl'>First Of All 🤞🏽</h2>
+              <div className="my-12 mt-20">
+                <h2 className='sm:text-3xl'>First Of All 🤞🏽</h2>
                 <p className='dark:text-[#bbbbbb] sm:text-lg'>
                   I am Israel Oluwapelumi, a creative engineer with passion for crafting 
                   high-performance, user-friendly, easy-to-use, and quality web platforms using my stack 
@@ -89,9 +89,9 @@ function App() {
               </div>
             </div>
             
-            <div className="skills mt-16">
-              <h2 className='headers sm:text-3xl'>My Skill Set 🤹</h2>
-              <div className="skill-icons flex flex-wrap justify-evenly gap-5 mt-8">
+            <div className="mt-16">
+              <h2 className='sm:text-3xl'>My Skill Set 🤹</h2>
+              <div className="skill-icons flex flex-wrap justify-evenly gap-5 mt-10">
                 <img className='mb-6' src={html} alt="" />
                 <img  className='mb-6' src={css} alt="" />
                 <img  className='mb-6' src={sass} alt="" />
@@ -103,12 +103,12 @@ function App() {
               </div>
             </div>
 
-            <div className="projects-container my-12">
-              <h2 className="headers sm:text-3xl">What I've cooked so far 👨‍🍳</h2>
+            <div className="my-20">
+              <h2 className="sm:text-3xl">What I've cooked so far 👨‍🍳</h2>
               <p className='dark:text-[#bbbbbb] sm:text-lg'>
                 I try my best to make the best “meals” and here are some delicious ones🍲...
               </p>
-              <div className="projects my-10 flex flex-col justify-center items-center">
+              <div className="my-10 flex flex-col justify-center items-center">
                 <div className='project dark:text-[#bbbbbb] sm:w-pw'>
                   <img src={movieApp} alt="" />
                   <h3>Movie App</h3>
@@ -132,7 +132,7 @@ function App() {
           </body>
         </div>
         
-        <footer className='bg-yellow text-white p-8 sm:p-14'>
+        <footer className='bg-yellow text-white p-10 sm:p-14'>
           <h2 className="text-white text-2xl sm:text-4xl">Friends now🤝🏽?</h2>
           <p className='my-2 sm:my-4 sm:text-lg'>
             Got a question, project or want to work together on something? Feel free to reach out👋🏽.  <br />
